5da0cf2df2175d292198bdf74be8f3697aabd638
Juan Borda <juanignacioborda@gmail.com>
2014-04-29 20:30:20 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+XMPP2CONSOLE	{#welcome}
+=====================
+This is a simple Node.js application that allows you to interact with a remote system via the xmpp protocol (aka Jabber,gtalk,etc )
+
+> NOTE:  Running commands thru this app is highly dangerous use it at your own risk
+
+Requirements:
+-------------
+you need [node.js][1] and npm
+
+Installation:
+------------
+npm install 
+
+Configuration:
+--------------
+
+see included:
+
+    config/config.json
+
+Getting Started:
+----------------
+prepparation: you will need:
+
+ - 1 xmpp server (openfire for local or private networks will do just fine, google will be fine too)
+ - an Account for your server bot like: server01@mynetwork.com or server01.mycomp@gmail.com (google apps accounts will work too)
+ - Your own account in the same server.
+ - you need to make this two accounts buddies (use whatever jabber client you like:pidgin, miranda im, gtalk, google hang outs) so they can see each other.
+
+Now you have all of the above you're ready to go.
+go to your server checkout the project or copy the files and execute it app.js like:
+
+    node app.js
+then the server will start an xmpp bot with the user (JID) and password provided in **config/config.json**
+
+Some usefull commands
+---------------------
+
+init: Will print you the hostname and the result of uname -a
+init executes a chain of commands that is defined in **config/config.json**
+
+
+
+ 
+  [1]: http://nodejs.org/
---
+++ app.js
@@ -0,0 +1,135 @@
+'use strict';
+/*
+ * on debian:
+ * 
+ * apt-get install libicu-dev
+ * 
+ * 
+ */
+
+console.log(".-------------------------------.");
+console.log("| * Starting XMPP BOT service * |");
+console.log("'-------------------------------'");
+
+var path = require('path');
+var fs = require('fs');
+var sh = require('execSync');
+var xmpp = require('./index')
+var argv = process.argv
+var baseDir = path.dirname(fs.realpathSync(__filename)) + '/';
+console.log("'LOADING CONFIG:'");
+var data = fs.readFileSync(baseDir + 'config/config.json');
+var config = JSON.parse(data);
+var listen = true;
+var client = new xmpp.Client(config.client);
+process.cwd(config.appsDir);
+client.on('online', function() {
+	console.log('online')
+	client.send(new xmpp.Element('presence', {}).c('show').t('chat').up().c(
+			'status').t(
+			(config.helloMsg) ? config.helloMsg : 'XMPP BOT ready for order'));
+	/*
+	 * add authorized users
+	 */
+	for(var i in config.enabledJIDs){
+		
+	var stanza = new xmpp.Element('presence', {type: 'subscribe', to: config.enabledJIDs[i]});
+	console.log('Suscribing:'+config.enabledJIDs[i]);
+    client.send(stanza);
+    }	
+})
+
+client.on('stanza', function(stanza) {
+	var result = "";
+	//if (stanza.is('presence')) console.log(stanza.attrs);
+	if (stanza.is('message') &&
+	// Important: never reply to errors!
+	(stanza.attrs.type !== 'error')) {
+		// console.log(stanza.getChild('body'));
+		// console.log('--------------------------------------------------');
+		// Swap addresses...
+		if (stanza.getChild('body')) {
+			var cmd_arr = stanza.getChildText('body').toString().split(' ');
+			var cmd = cmd_arr[0];
+			if (listen) {
+
+				console.log(stanza.from + ' > ' + cmd_arr.join(' '));
+
+				switch (cmd) {
+				case "sleep":
+					/*
+					 * Bot will stop listening for commands
+					 */
+					listen = false;
+					result = "going sleep for a while";
+					break;
+				case "poweroff":
+					/*
+					 * power off the bot process
+					 */
+					console.log('Bye...!!!');
+					process.exit();
+					break;
+				case "halt":
+					break;
+				default:
+					/*
+					 * Check if is a predefined command
+					 */
+					if (config.predefCmds[cmd]) {
+						for ( var i in config.predefCmds[cmd]) {
+							result += execute_cmd(config.predefCmds[cmd][i]);
+						}
+					} else {
+						cmd = cmd_arr.join(' ');
+						result = 'Result:\n' + execute_cmd(cmd);
+					}
+					break;
+				}
+				send_msg(stanza.attrs.from, result);
+			}
+			if (cmd == "wakeup") {
+				listen = true;
+				send_msg(stanza.attrs.from, 'ready for commands again');
+			}
+
+			// and send back
+			// console.log('Sending response: ' + st1.root().toString());
+
+		}
+	}
+})
+
+client.on('error', function(e) {
+	console.error(e)
+});
+function send_msg(to, msg) {
+	var st1 = new xmpp.Element('message', {
+		"to" : to,
+		"type" : "chat"
+	}).c('body').t(msg);
+	client.send(st1);
+}
+
+function execute_cmd(cmd) {
+	var result = sh.exec(cmd);
+	return result.stdout
+}
+
+function readJSON(file, callback) {
+	try {
+		var data = fs.readFileSync(file), myObj;
+		try {
+			myObj = JSON.parse(data);
+			if (callback) {
+				callback(myObj, null);
+			} else {
+				return myObj;
+			}
+		} catch (err) {
+			callback({}, err);
+		}
+	} catch (err) {
+		callback({}, err);
+	}
+}
